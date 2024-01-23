2d9235573c9141cfffd414e5726eed28d0a176a2
Samuel Seguins <samuelseguins@gmail.com>
2024-01-22 23:22:58 -0300

    atualização de desing(adicionado lampada-off)

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const range = document.getElementById('range');
-const box = document.querySelector('.content');
+const boxWhite = document.querySelector('.content');
+const boxBlack = document.querySelector('.content-black');
 const extBox = document.querySelector('.box');
+
 const lamp = document.querySelector('.imgLamp');
+const lampBlack = document.querySelector('.lampOff');
 
 function nivel(){
     let valor = Number(range.value)
-    box.style.height = (valor) + '%' ;
+    boxWhite.style.height = (valor) + '%' ;
+    boxBlack.style.height = (100-valor) + '%'
     
-    document.querySelector('.cnt').style.opacity = valor/100+0.08;
+    lampOff.style.opacity = 100-(valor) + '%'
     lamp.style.opacity = valor/100;
+
+    document.querySelector('.cnt').style.opacity = (valor+8)+'%';
 
     if(valor<=25){
         lamp.style.display = 'none';
     }else{
         lamp.style.display = 'block';
+    }
+
+    if(valor>=72){
+        lampOff.style.display = 'none'
+    }else{
+        lampOff.style.display = 'block';
     }
 }
 
